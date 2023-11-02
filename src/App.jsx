fd9cd580bbdb4f430c9dbe1b0a18e1c099dd424b
Kike Gómez <kike.gomez.uy@gmail.com>
2023-11-02 20:33:34 -0300

    Carga Resolución del Parcial

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,85 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Card from "./Components/Card";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Carga de estudiantes</h1>
-      <form></form>
+  
+  // States
+  const [student, setStudent] = useState({
+    name: "",
+    last_name: "",
+    birth_date: "",
+  });
 
-      <Card />
+  const [show, setShow] = useState(false);
+  const [error, setError] = useState(false);
+
+  //Validators
+  function isValidInput(inputText) {
+    const letterRegex = /^[A-Za-záéíóúüÁÉÍÓÚÜñÑ\s]+$/;
+    return letterRegex.test(inputText);
+  }
+
+  //Handlers
+  const handleChange = (e, objProp) =>
+    setStudent({ ...student, [objProp]: e.target.value.trimStart() });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(student);
+    if (
+      student.name.length > 2 &&
+      isValidInput(student.name) &&
+      student.last_name.length > 5 &&
+      isValidInput(student.last_name)
+    ) {
+      setShow(true);
+      setError(false);
+    } else {
+      setError(true);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Carga de Estudiantes</h1>
+      {!show && (
+        <form className="form-student" onSubmit={handleSubmit}>
+          <label>Nombre</label>
+          <input
+            type="text"
+            value={student.name || ""}
+            onChange={(e) => handleChange(e, "name")}
+            placeholder="Mínimo 3 letras. Sin números ni caracteres epeciales"
+          />
+          <label>Apellido</label>
+          <input
+            type="text"
+            value={student.last_name || ""}
+            onChange={(e) => handleChange(e, "last_name")}
+            placeholder="Mínimo 6 letras. Sin números ni caracteres epeciales"
+          />
+          <label>Fecha de Nacimiento</label>
+          <input
+            type="date"
+            value={student.birth_date || ""}
+            onChange={(e) => handleChange(e, "birth_date")}
+          />
+          <button>Registrarse</button>
+        </form>
+      )}
+      {show && (
+        <Card student={student}/>
+      )}
+      {error && (
+        <div className="error">
+          <h4>Por favor chequea que la información sea correcta</h4>
+          <ul>
+            <li>El Nombre debe tener al menos 3 caracteres alfabéticos</li>
+            <li>El Apellido debe tener al menos 6 caracteres alfabéticos</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,8 +1,47 @@
+import { welcome, firework, fireworkBox } from "./Card.module.css";
+/* eslint-disable react/prop-types */
+const Card = ({ student }) => {
+    // Desestructuro el object
+  const { name, last_name, birth_date } = student;
 
-const Card = () => {
+    // Custom functions
+  function setAge(date) {
+    const birth_date = date.birth_date;
+    const parts = birth_date.split("-");
+    const year = parseInt(parts[0], 10);
+    const month = parseInt(parts[1], 10);
+    const day = parseInt(parts[2], 10);
+
+    const today = new Date();
+    
+    let age = today.getFullYear() - year;
+
+    if (
+      today.getMonth() < month - 1 ||
+      (today.getMonth() === month - 1 && today.getDate() < day)
+    ) {
+      age--;
+    }
+
+    return age;
+  }
+
+  const age = setAge({ birth_date });
+
   return (
-    <div>Hola, soy una tarjeta</div>
-  )
-}
+    <>
+      <div className={welcome}>
+        <h2>Hola{" "} <span>{name} {last_name}</span> 👋</h2>
+        <p>Con la fecha que ingresaste, identificamos que tienes: {age} años</p>
+        <p>Que hermosa edad, muchas gracias por sumarte y ...</p>
+        <h2>🥳 Bienvenido al equipo!! 🎉</h2>
+      </div>
+      <div className={fireworkBox}>
+        <div className={firework}></div>
+        <div className={firework}></div>
+      </div>
+    </>
+  );
+};
 
-export default Card
+export default Card;
